318a4416b2f3379d53c5eb5b25e1bbf85a9c2d44
wangchun <2838138608@qq.com>
2020-05-06 20:57:38 +0800

    test let const var using

--- ES6TEST/dist/index.js
+++ ES6TEST/dist/index.js
@@ -1,4 +1,21 @@
 "use strict";
 
-var a = 2;
+var a = "wangmike";
+//console.log(a);
+/*
+window.onload = function () {
+    console.log(a);
+}*/
+//let a = 1;
+{
+    var _a = "hhhhh";
+    console.log(_a);
+}
 console.log(a);
+for (var i = 0; i < 10; i++) {
+    console.log("我是来自i循环的数据：：：" + i);
+}
+console.log(i);
+for (i; i < 100; i++) {
+    console.log("我是来自i循环的数据：：：" + i);
+}
--- ES6TEST/src/index.js
+++ ES6TEST/src/index.js
@@ -1,2 +1,20 @@
-let a = 2;
+const a = "wangmike";
+//console.log(a);
+/*
+window.onload = function () {
+    console.log(a);
+}*/
+//let a = 1;
+{
+    let a = "hhhhh"
+    console.log(a)
+}
 console.log(a);
+for (var i = 0 ; i < 10 ; i++){
+    console.log("我是来自i循环的数据：：：" + i);
+}
+console.log(i)
+for (i ; i < 100 ; i++){
+    console.log("我是来自i循环的数据：：：" + i);
+}
+
