5a1385e7257d2a1d255fc8083f014b4085f3a61f
wangchun <2838138608@qq.com>
2020-05-18 06:59:15 +0800

    初始化小程序代码

---
+++ wechat-applets/movie-detail-applets/pages/posts/post-detail/post-detail.js
@@ -0,0 +1,52 @@
+import localData from "../../../data/local-data";
+Page({
+    data: {
+        collectPostId:""
+    },
+    onLoad: function (options) {
+        let currentPostId = options.postId
+        //向全局变量赋值
+        this.data.collectPostId = currentPostId;
+        let postDetail = localData[currentPostId];
+        this.setData({
+            postDetail: postDetail
+        })
+        //缓存收藏设置
+        let postsCollected = wx.getStorageSync("post_collection");
+        if(postsCollected){
+            //获取当前文章收藏情况
+            let postCollected = postsCollected[currentPostId];
+            this.setData({
+                collected: postCollected
+            })
+        }else{
+            postsCollected = [];
+            postsCollected[currentPostId] = false;
+            wx.setStorageSync("post_collection",postsCollected);
+        }
+    },
+    /**
+     * 收藏功能
+     */
+    onCollectionTap(event){
+        //获取缓存中的变量
+        let postsCollected = wx.getStorageSync("post_collection");
+        //获取当前详情是否被收藏
+        let postCollected = postsCollected[this.data.collectPostId];
+        //取相反值
+        postCollected = !postCollected;
+        postsCollected[this.data.collectPostId] = postCollected;
+        //更新数据绑定变量，从而实现切换图片的效果
+        this.setData({
+            collected: postCollected
+        });
+        //更新文章是否缓存的值
+        wx.setStorageSync("post_collection",postsCollected);
+    },
+    /**
+     * 分享功能
+     */
+    onShareTap(event){
+        wx.removeStorageSync("key");
+    }
+});
---
+++ wechat-applets/movie-detail-applets/pages/welcome/welcome.js
@@ -0,0 +1,20 @@
+Page({
+    onTap(){
+        //小程序页面跳转，不可以回退，触发的是onUnload事件
+        wx.redirectTo({
+            url: "../posts/posts"
+        });
+        //小程序页面跳转，可以回退，触发的是onHide事件
+      /*  wx.navigateTo({
+            url: "../posts/posts"
+        })*/
+    },
+
+    onHide(){
+        console.log("welcome page is onhide")
+    },
+    //wx.re
+    onUnload(){
+        console.log("welcome page is onUnload")
+    }
+})
