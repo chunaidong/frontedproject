3450dce93016149dce298c035558529aa763f3e4
wangchun <2838138608@qq.com>
2020-05-19 09:43:47 +0800

    新增电影模板页面

--- wechat-applets/movie-detail-applets/pages/movies/movies.js
+++ wechat-applets/movie-detail-applets/pages/movies/movies.js
@@ -1,5 +1,22 @@
+var app = getApp();
 Page({
     data:{
 
+    },
+    onLoad(options){
+        wx.request({
+            url: `${app.globalProperties.dobanBase}/v2/movie/top250`,
+            data:{},
+            method:'GET',
+            header:{
+              "Content-type":"application/json"
+            },
+            success: res => {
+                console.log(res);
+            },
+            fail: error => {
+                console.log(error)
+            }
+        })
     }
 })
