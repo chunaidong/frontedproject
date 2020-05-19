1bd54dfd3c412c697302ca24b0086d37313e12bb
wangchun <2838138608@qq.com>
2020-05-19 16:23:41 +0800

    新增滑动加载数据

--- wechat-applets/movie-detail-applets/pages/movies/more-movie/more-movie.js
+++ wechat-applets/movie-detail-applets/pages/movies/more-movie/more-movie.js
@@ -2,7 +2,11 @@ var app = getApp();
 var utils = require("../../utils/utils.js");
 Page({
     data:{
-        category:""
+        category:"",
+        movies:{},
+        requestUrl:"",
+        totalCount:0,
+        isEmpty:true
     },
     onLoad(options){
         this.setData({
@@ -22,6 +26,17 @@ Page({
         }
         let url = `${app.globalProperties.dobanBase}${dataUrl}`
         utils.http(url,this.processMovieData);
+        this.setData({
+            requestUrl:url
+        })
+    },
+    /**
+     * 滑动到底部触发的事件
+     */
+    onScroll(event){
+        let nextUrl = `${this.data.requestUrl}?start=${this.data.totalCount}&count=20`;
+        utils.http(nextUrl,this.processMovieData);
+        wx.showNavigationBarLoading();
     },
     processMovieData(doubanMovieData){
         let movies = [];
@@ -36,9 +51,20 @@ Page({
             }
             movies.push(tmp);
         }
+        let totalMovies = {};
+        if(this.data.isEmpty){
+            totalMovies = movies;
+            this.setData({
+                isEmpty:false
+            })
+        }else{
+            totalMovies = this.data.movies.concat(movies);
+        }
         this.setData({
-            movies: movies
+            movies: totalMovies,
+            totalCount: this.data.totalCount + 20
         });
+        wx.hideNavigationBarLoading();
     },
     onReady(event){
         wx.setNavigationBarTitle({
